d460713de5315bc237cdc016ee42d2f41c6b361a
sanjay yadav <sanjaykumaryadav72658@gmail.com>
2021-01-26 17:50:35 +0530

    added logger

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -1,32 +1,51 @@
 package com.springboot.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.springboot.model.ApiResponse;
 import com.springboot.model.User;
 import com.springboot.model.UserDto;
 import com.springboot.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	
+	Logger log = Logger.getLogger(UserController.class.getName());
 
     @Autowired
     private UserService userService;
 
     @PostMapping
     public ApiResponse<User> saveUser(@RequestBody UserDto user){
-        return new ApiResponse<>(HttpStatus.OK.value(), "User saved successfully.",userService.save(user));
+    	log.info("UserController:saveUser()::START");
+         ApiResponse<User> response=new ApiResponse<>(HttpStatus.OK.value(), "User saved successfully.",userService.save(user));
+        log.info("UserController:saveUser()::END");
+        return response;
+         
     }
 
     @GetMapping
     public ApiResponse<List<User>> listUser(){
-        return new ApiResponse<>(HttpStatus.OK.value(), "User list fetched successfully.",userService.findAll());
+    	log.info("UserController:listUser()::START");
+    	ApiResponse<List<User>> response= new ApiResponse<>(HttpStatus.OK.value(), "User list fetched successfully.",userService.findAll());
+    	 log.info("UserController:listUserr()::END");
+         return response;
+          
     }
 
     @GetMapping("/{id}")
